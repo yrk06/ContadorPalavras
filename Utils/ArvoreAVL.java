adb667b808adabbf782a02413ad3bf1f64b00a9f
Yerik <yerik.koslowski@gmail.com>
2021-05-18 08:35:45 -0300

    Adicionado Estrutura de balanceamento

--- Utils/ArvoreAVL.java
+++ Utils/ArvoreAVL.java
@@ -5,19 +5,103 @@ public class ArvoreAVL extends ArvoreBinaria  {
     public ArvoreAVL(){
     }
 
-    /*
-    Calcular o balanceamento em 1 nó:
-        Altura Direita - Altura Esquerda
-    */
-
-    public int[] fator_balanceamento(){
-        Node[] Nodes = this.percorrer_preorder();
-        int[] balancing = new int[Nodes.length];
-
-        for(int idx = 0; idx < Nodes.length;idx++){
-            
+    @Override
+    public void insert(String info){
+        if(root == null){
+            Node nodeToAdd = new Node(info);
+            root = nodeToAdd;
+            return;
         }
-
-        return balancing;
+        root.insert_element(info);
+        balanceamento(root);
     }
+    
+    public int checar_balanceamento(Node x) {
+		// Node l = x.left;
+		// Node r = x.right;
+		if (x == null) {
+			return 0;
+		}
+        int left_size = 0;
+        int right_size = 0;
+		if (x.left != null) {
+            left_size = x.left.size();
+		}
+		if (x.right != null) {
+			right_size = x.right.size();
+		}
+		return (left_size - right_size);
+	}
+
+	public void balanceamento(Node x) {
+        int fator_balanceamento = checar_balanceamento(x);
+
+        if (Math.abs(fator_balanceamento) > 1){
+            if (fator_balanceamento > 0){
+                //Desbalanceado pra esquerda
+                if (Math.abs(checar_balanceamento(x.left)) > 1){
+                    balancear(x.left);
+                }
+                else{
+                    balancear(x);
+                }
+            } else {
+                //Desbalanceado para direita
+                if (Math.abs(checar_balanceamento(x.right)) > 1){
+                    balancear(x.right);
+                }
+                else{
+                    balancear(x);
+                }
+            }
+        }
+	}
+
+	public void balancear(Node x) {
+		if (x == null) {
+			return;
+		}
+		if (checar_balanceamento(x) > 1) {
+			
+            //Peca a direita vira o Nroot
+            Node newRoot = x.left;
+            //O que ta a direita do new Root vai ficar a esquerda do root atual
+            Node nRootOldRight = newRoot.right;
+            x.right = nRootOldRight;
+
+            //O root atual fica a direita do new root
+            newRoot.right = x;
+
+            //caso especial
+            if (x == root){
+                root = newRoot;
+            }
+            
+            // rotação direita
+			/*Node q = x.left;
+			Node aux = q.right;
+			q.right = x;
+			x.left = aux;*/
+
+		} else {
+            //Peca a direita vira o Nroot
+            Node newRoot = x.right;
+            //O que ta a esquerda do new Root vai ficar a direita do root atual
+            Node nRootOldLeft = newRoot.left;
+            x.right = nRootOldLeft;
+
+            //O root atual fica a esquerda do new root
+            newRoot.left = x;
+
+            //caso especial
+            if (x == root){
+                root = newRoot;
+            }
+			// rotação esquerda
+			/*Node q = x.right;
+			Node aux = q.left;
+			q.left = x;
+			x.right = aux;*/
+		}
+	}
 }
